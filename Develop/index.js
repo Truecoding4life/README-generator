799dfe9aa1316f550d0a1dd17e3624c8bf01d688
Thai Nghiem <thainghiem@Thais-MacBook-Air.local>
2023-10-19 23:23:35 -0700

    inquirer working

--- Develop/index.js
+++ Develop/index.js
@@ -5,59 +5,74 @@ const fs = require("fs");
 
 const generateMarkdown = require("./utils/generateMarkdown")
 // TODO: Create an array of questions for user input
-const questions = inquirer
+const questions = [{
+  name: 'title',
+  type: 'input',
+  message: 'Enter Project Title to Continue',
+
+},
+{
+  name: 'description',
+  type: 'input',
+  message: 'Enter your description to continue',
+
+}, {
+  name: 'installation',
+  type: 'input',
+  message: 'Enter your installation instruction to continue',
+},
+{
+  name: 'usage',
+  type: 'input',
+  message: 'Enter your Usage Information to continue',
+
+},
+{
+  name: 'contri',
+  type: 'input',
+  message: 'Enter your contributor to continue',
+
+},
+{
+  name: 'test',
+  type: 'input',
+  message: 'OPTIONAL | Enter your Test to continue | Press Enter to Skip',
+},
+]
+
+inquirer
   .prompt([
-    {name: 'title',
-      type: 'input',
-      message: 'Enter Project Title to Continue',
-      
-    },
-    {
-        name: 'description',
-      type: 'input',
-      message: 'Enter your description to continue',
-      
-    },
-    {
-        name: 'choices',
-        type: 'list',
-        message: 'Select next section to add to your README:',
-        choices: [ "Features", "Usage", "User Interface", ]
-      },
+    questions[0],questions[1],questions[2],questions[3],questions[4],questions[5]
   ])
+  .then((response) => {
+    console.log(response)
+    // writeToFile("README.md", generateMarkdown(response))
+    if (response.test=='') {
+      delete questions.test
+      console.log("User skipped test")
+    }
 
+  })
+  // .prompt([
+  //   questions[2],questions[3]
+  // ]);
 
-.then((response) => {
-    const choice = response.choices;
-            if(choice == "Feature"){
-                                console.log("User Selected Features")
-            }
-            if(choice== "Usage"){
-                                    console.log("User Selected Usage")
-            }
-
-            if(choice== "User Interface") {
-                    console.log("User Selected Interface")
-            }
-    });
-//   .then((response) => {
-//     console.log(response)
-//     response.confirm === response.password
-//       ? console.log('Success!')
-//       : console.log('You forgot your password already?!')
-//   }
-  ;
+;
 
 //* add type, name, and message for each question
 //* add questions:  added readme sections (description, usage, license, etc)
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {
-//* use FS module to write data and fileName to readme file
+  fs.writeFile(fileName, data, (err) =>
+    err ? console.error(err) : console.log('Success!')
+  );
 }
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 // Function call to initialize app
 init();
 //* use inquiter to display questios to user
 //* call generateMarkdown funcoin on the data collected
 //* take what is returned to us, then pass it to the writeToFile funciton
+
+
